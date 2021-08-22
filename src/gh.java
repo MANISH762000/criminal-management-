2d34be78281990303ed0127b1e5c5c7c6bcd12f4
MANISH762000 <43494119+MANISH762000@users.noreply.github.com>
2021-08-23 02:41:38 +0530

    Add files via upload

---
+++ src/gh.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author itlab3
+ */
+public class gh {
+    
+    public static void main(String args[])
+    {
+        System.out.println("%d");
+                
+    }
+    
+}
